d096612b122900f92d217b4ef440f8f273749bc9
adityashah6 <60235219+adityashah6@users.noreply.github.com>
2023-04-23 20:45:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ from food_item_info import *
 #from lottie import load_animation
 
 
+# ============================ Global Variables ============================
 nlp = spacy.load('output/model-last')  # Our custom named entity recognition model
-
+FONT = "Consolas"
 
 
 # ====================== NLP Functionalities ======================
@@ -53,9 +54,8 @@ def load_lottiefile(path: str):
 def main():
     #st.write('<h1 style="font-size: 40px;">FoodKeeper Named Research Project</h1>', unsafe_allow_html=True)
     text = "FoodKeeper Research Project"
-    font = "Consolas"
     font_size = "42px"
-    st.markdown(f"<span style='font-family:{font};font-size:{font_size}'>{text}</span>", unsafe_allow_html=True)
+    st.markdown(f"<span style='font-family:{FONT};font-size:{font_size}'>{text}</span>", unsafe_allow_html=True)
 
     menu_options = ['Home', 'NER']
     # choice = st.sidebar.selectbox('Menu', menu_options)
@@ -86,9 +86,8 @@ def main():
         # show_lottie_animation(animation, speed=1)
 
         text = "Overview"
-        font = "Consolas"
         font_size = "30px"
-        st.markdown(f"<span style='font-family:{font};font-size:{font_size}'>{text}</span>", unsafe_allow_html=True)
+        st.markdown(f"<span style='font-family:{FONT};font-size:{font_size}'>{text}</span>", unsafe_allow_html=True)
 
         text = '''
             Welcome to our website where we shed light on the critical issue of food waste in the United States. 
@@ -98,16 +97,14 @@ def main():
             80% of Americans. At our website, we aim to raise awareness about food waste and provide helpful tips and resources to reduce it.
 
         '''
-        font = "Consolas"
         font_size = "15px"
-        st.markdown(f"<p style='font-family:{font};font-size:{font_size}'>{text}", unsafe_allow_html=True)
+        st.markdown(f"<p style='font-family:{FONT};font-size:{font_size}'>{text}", unsafe_allow_html=True)
         # st.markdown('Welcome to our website where we shed light on the critical issue of food waste in the United States. Did you know that approximately 30-40% of the food supply is wasted in the US, which amounts to about 133 billion pounds? Unfortunately, consumers also play a significant role in contributing to food waste. In fact, the average household wastes nearly 32% of the food it buys. One major reason for this waste is date label confusion, which leads to the premature disposal of food by 80% of Americans. At our website, we aim to raise awareness about food waste and provide helpful tips and resources to reduce it.')
         
         
         text = "How to Use"
-        font = "Consolas"
         font_size = "30px"
-        st.markdown(f"<span style='font-family:{font};font-size:{font_size}'>{text}</span>", unsafe_allow_html=True)
+        st.markdown(f"<span style='font-family:{FONT};font-size:{font_size}'>{text}</span>", unsafe_allow_html=True)
 
 
         text = '''
@@ -116,16 +113,14 @@ def main():
         refrigerate, and freeze specific foods to keep them fresh and delicious for as long as possible. Whether you're a seasoned chef or a beginner 
         cook, our NLP technology makes food preservation easier than ever before.
         '''
-        font = "Consolas"
         font_size = "15px"
-        st.markdown(f"<p style='font-family:{font};font-size:{font_size}'>{text}", unsafe_allow_html=True)
+        st.markdown(f"<p style='font-family:{FONT};font-size:{font_size}'>{text}", unsafe_allow_html=True)
 
     elif choice == 'NER':
         
         text = "Named Entity Recognition: Foods"
-        font = "Consolas"
         font_size = "25px"
-        st.markdown(f"<span style='font-family:{font};font-size:{font_size}'>{text}</span>", unsafe_allow_html=True)
+        st.markdown(f"<span style='font-family:{FONT};font-size:{font_size}'>{text}</span>", unsafe_allow_html=True)
         
         
         #st.subheader('Named Entity Recognition: Foods')
@@ -135,14 +130,16 @@ def main():
             docx = nlp(raw_text)
             types = ['Pantry', 'Refrigerate', 'Freeze']
 
-            spacy_streamlit.visualize_ner(docx, labels=nlp.get_pipe("ner").labels)
+            spacy_streamlit.visualize_ner(docx, labels=nlp.get_pipe("ner").labels,)
             print('Entities Found: ' + str(docx.ents))
 
             # ------------ Generating responses, if it is relevant to food ------------
             message = raw_text
             preprocessed_message = preProcess(message)
 
-            st.subheader('Here are some Pantry, Refrigerate, and Freezing tips according to the U.S. Department of Food Safety and Inspection:')
+            text = "Here are some Pantry, Refrigerate, and Freezing tips according to the U.S. Department of Food Safety and Inspection:"
+            font_size = "25px"
+            st.markdown(f"<span style='font-family:{FONT};font-size:{font_size}'>{text}</span>", unsafe_allow_html=True)
             for e in docx.ents:
                 item = e.text
                 st.markdown(f"<h4>{item}</h4>", unsafe_allow_html=True)
